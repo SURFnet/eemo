e3705e5d119705546650903ee9651d6aad044d46
rijswijk <rijswijk@36d02cbb-6ffa-4976-ba29-6cdf1a1a0648>
2011-01-30 14:58:35 +0000

    Working on TCP handler

---
+++ src/tcp_handler.c
@@ -0,0 +1,223 @@
+/* $Id$ */
+
+/*
+ * Copyright (c) 2010-2011 SURFnet bv
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * The Extensible Ethernet Monitor (EEMO)
+ * TCP packet handling
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "eemo.h"
+#include "tcp_handler.h"
+#include "tcp_handler.h"
+
+/* The linked list of TCP packet handlers */
+eemo_tcp_handler* tcp_handlers = NULL;
+
+/* Find an IP handler for the specified type */
+eemo_tcp_handler* eemo_find_tcp_handler(u_short srcport, u_short dstport)
+{
+	eemo_tcp_handler* current = tcp_handlers;
+
+	while (current != NULL)
+	{
+		if (((current->srcport == TCP_ANY_PORT) || (current->srcport == srcport)) &&
+		    ((current->dstport == TCP_ANY_PORT) || (current->dstport == dstport)))
+		{
+			return current;
+		}
+
+		current = current->next;
+	}
+
+	return NULL;
+}
+
+/* Convert TCP packet header to host byte order */
+void eemo_tcp_ntoh(eemo_hdr_tcp* hdr)
+{
+	hdr->tcp_srcport	= ntohs(hdr->tcp_srcport);
+	hdr->tcp_dstport	= ntohs(hdr->tcp_dstport);
+	hdr->tcp_seqno		= ntohl(hdr->tcp_seqno);
+	hdr->tcp_ackno		= ntohl(hdr->tcp_ackno);
+	hdr->tcp_win		= ntohs(hdr->tcp_win);
+	hdr->tcp_chksum		= ntohs(hdr->tcp_chksum);
+	hdr->tcp_urgent		= ntohs(hdr->tcp_urgent);
+}
+
+/* Handle a TCP packet */
+eemo_rv eemo_handle_tcp_packet(eemo_packet_buf* packet, eemo_ip_packet_info ip_info)
+{
+	eemo_hdr_tcp* hdr = NULL;
+
+	/* Check minimum length */
+	if (packet->len < sizeof(eemo_hdr_tcp))
+	{
+		/* TCP packet is malformed */
+		return ERV_MALFORMED;
+	}
+
+	/* Take the header from the packet */
+	hdr = (eemo_hdr_tcp*) packet->data;
+
+	/* Convert the header to host byte order */
+	eemo_tcp_ntoh(hdr);
+
+	/* See if there is a handler given the source and destination port for this packet */
+	eemo_tcp_handler* handler = eemo_find_tcp_handler(hdr->tcp_srcport, hdr->tcp_dstport);
+
+	if ((handler != NULL) && (handler->handler_fn != NULL))
+	{
+		eemo_rv rv = ERV_OK;
+		eemo_packet_buf* tcp_data = 
+			eemo_pbuf_new(&packet->data[sizeof(eemo_hdr_tcp)], packet->len - sizeof(eemo_hdr_tcp));
+
+		if (tcp_data == NULL)
+		{
+			return ERV_MEMORY;
+		}
+
+		rv = (handler->handler_fn)(tcp_data, ip_info, hdr->tcp_srcport, hdr->tcp_dstport);
+
+		eemo_pbuf_free(tcp_data);
+
+		return rv;
+	}
+
+	return ERV_SKIPPED;
+}
+
+/* Register an TCP handler */
+eemo_rv eemo_reg_tcp_handler(u_short srcport, u_short dstport, eemo_tcp_handler_fn handler_fn)
+{
+	eemo_tcp_handler* new_handler = NULL;
+
+	/* Check if a handler for the specified ports already exists */
+	if (eemo_find_tcp_handler(srcport, dstport) != NULL)
+	{
+		/* A handler for this type has already been registered */
+		return ERV_HANDLER_EXISTS;
+	}
+
+	/* Create a new handler entry */
+	new_handler = (eemo_tcp_handler*) malloc(sizeof(eemo_tcp_handler));
+
+	if (new_handler == NULL)
+	{
+		/* Not enough memory */
+		return ERV_MEMORY;
+	}
+
+	new_handler->srcport = srcport;
+	new_handler->dstport = dstport;
+	new_handler->handler_fn = handler_fn;
+	new_handler->next = NULL;
+
+	/* Register the new handler */
+	eemo_tcp_handler* current = tcp_handlers;
+
+	if (current == NULL)
+	{
+		/* This is the first registered handler */
+		tcp_handlers = new_handler;
+	}
+	else
+	{
+		/* Append this handler to the list */
+		while (current->next != NULL) current = current->next;
+
+		current->next = new_handler;
+	}
+
+	return ERV_OK;
+}
+
+/* Unregister an TCP handler */
+eemo_rv eemo_unreg_tcp_handler(u_short srcport, u_short dstport)
+{
+	eemo_tcp_handler* current = tcp_handlers;
+	eemo_tcp_handler* prev = NULL;
+
+	while (current != NULL)
+	{
+		if (((current->srcport == TCP_ANY_PORT) || (current->srcport == srcport)) &&
+		    ((current->dstport == TCP_ANY_PORT) || (current->dstport == dstport)))
+		{
+			/* Found the handler to delete, remove it from the chain */
+			if (prev == NULL)
+			{
+				tcp_handlers = current->next;
+			}
+			else
+			{
+				prev->next = current->next;
+			}
+
+			free(current);
+
+			return ERV_OK;
+		}
+
+		prev = current;
+		current = current->next;
+	}
+
+	/* No such handler exists */
+	return ERV_NO_HANDLER;
+}
+
+/* Initialise IP handling */
+eemo_rv eemo_init_tcp_handler(void)
+{
+	/* Register TCP packet handler */
+	return eemo_reg_ip_handler(IP_TCP, &eemo_handle_tcp_packet);
+}
+
+/* Clean up */
+void eemo_tcp_handler_cleanup(void)
+{
+	/* Clean up the list of TCP packet handlers */
+	eemo_tcp_handler* to_delete = NULL;
+	eemo_tcp_handler* current = tcp_handlers;
+	tcp_handlers = NULL;
+
+	while (current != NULL)
+	{
+		to_delete = current;
+		current = current->next;
+
+		to_delete->next = NULL;
+		free(to_delete);
+	}
+
+	/* Unregister the IP handler for TCP packets */
+	eemo_unreg_ip_handler(IP_TCP);
+}
+
---
+++ src/tcp_handler.h
@@ -0,0 +1,114 @@
+/* $Id$ */
+
+/*
+ * Copyright (c) 2010-2011 SURFnet bv
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHIP
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * The Extensible IP Monitor (EEMO)
+ * TCP packet handling
+ */
+
+#ifndef _EEMO_TCP_HANDLER_H
+#define _EEMO_TCP_HANDLER_H
+
+#include <pcap.h>
+#include <netdb.h>
+#include "eemo.h"
+#include "eemo_packet.h"
+#include "ip_handler.h"
+
+#define IP_TCP	0x0006
+
+#pragma pack(push, 1)
+/* TCP packet header */
+typedef struct
+{
+	u_short	tcp_srcport;	/* source port */
+	u_short tcp_dstport;	/* destination port */
+	u_int   tcp_seqno;	/* sequence number */
+	u_int	tcp_ackno;	/* acknowledgement number */
+	u_char	tcp_ofs;	/* data offset */
+	u_char	tcp_flags;	/* flags */
+	u_short	tcp_win;	/* window size */
+	u_short tcp_chksum;	/* checksum */
+	u_short	tcp_urgent;	/* urgent pointer */
+}
+eemo_hdr_tcp;
+#pragma pack(pop)
+
+/* TCP flags */
+#define TCP_CWR		0x80
+#define TCP_ECE		0x40
+#define TCP_URG		0x20
+#define TCP_ACK		0x10
+#define TCP_PSH		0x08
+#define TCP_RST		0x04
+#define TCP_SYN		0x02
+#define TCP_FIN		0x01
+
+/* TCP packet information */
+typedef struct
+{
+	u_short	srcport;	/* source port */
+	u_short	dstport;	/* destination port */
+	u_int	seqno;		/* sequence number */
+	u_int	ackno;		/* acknowledgement number */
+	u_char	flags;		/* flags */
+	u_short	winsize;	/* window size */
+	u_short	urgptr;		/* urgent pointer */
+}
+eemo_tcp_packet_info;
+
+/* Defines a handler for TCP packets */
+typedef eemo_rv (*eemo_tcp_handler_fn) (eemo_packet_buf*, eemo_ip_packet_info, eemo_tcp_packet_info);
+
+/* Defines an TCP handler record */
+#define TCP_ANY_PORT		0
+
+typedef struct eemo_tcp_handler
+{
+	u_short				srcport;	/* which source port, 0 = any */
+	u_short				dstport;	/* which destination port, 0 = any */
+	eemo_tcp_handler_fn		handler_fn;	/* handler function */
+	struct eemo_tcp_handler*	next;		/* next handler in the list */
+}
+eemo_tcp_handler;
+
+/* Register an TCP handler */
+eemo_rv eemo_reg_tcp_handler(u_short srcport, u_short dstport, eemo_tcp_handler_fn handler_fn);
+
+/* Unregister an TCP handler */
+eemo_rv eemo_unreg_tcp_handler(u_short srcport, u_short dstport);
+
+/* Initialise TCP handling */
+eemo_rv eemo_init_tcp_handler(void);
+
+/* Clean up */
+void eemo_tcp_handler_cleanup(void);
+
+#endif /* !_EEMO_TCP_HANDLER_H */
+
