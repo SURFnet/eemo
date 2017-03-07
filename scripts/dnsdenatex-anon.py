a8d1ce539acfe35e62d1d2aa46e0d94fa41c13c3
Ubuntu <rijswijk@dumpster.surfdomeinen.nl>
2017-03-07 09:04:07 +0000

    Updated anonymisation script

--- scripts/dnsdenatex-anon.py
+++ scripts/dnsdenatex-anon.py
@@ -15,12 +15,20 @@ def anonymise_dnsdenatex(in_csv, out_csv, salt):
 	in_fd = open(in_csv, 'r')
 	out_fd = open(out_csv, 'w')
 
+	field_count = 0
+
 	header = in_fd.readline()
 
 	header = header.strip('\n')
 	header = header.strip('\r')
 
-	if header != 'timestamp,client_ip,ip_ipid,udp_srcport,dns_qid,dns_qtype,dns_qclass,dns_qname,dns_edns0,dns_edns0_do,dns_edns0_maxsize':
+	if header == 'timestamp,client_ip,ip_ipid,ip_ttl,udp_srcport,dns_qid,dns_qtype,dns_qclass,dns_qname,dns_edns0,dns_edns0_do,dns_edns0_maxsize':
+		field_count = 12
+		print 'This is a file containing queries'
+	elif header == 'timestamp,client_ip,ip_ipid,ip_ttl,udp_dstport,dns_qid,dns_qtype,dns_qclass,dns_qname,first_response_ttl,dns_edns0,dns_edns0_do,dns_edns0_maxsize':
+		field_count = 13
+		print 'This is a file containing responses'
+	else:
 		print 'Invalid CSV header in {}'.format(in_csv)
 		in_fd.close()
 		out_fd.close()
@@ -30,8 +38,10 @@ def anonymise_dnsdenatex(in_csv, out_csv, salt):
 
 	line_ct = 0
 
-	# fields[1] = src_ip
-	# fields[7] = qname
+	# fields[1] = client_ip
+	client_ip_index = 1
+	# fields[8] = qname
+	qname_index = 8
 	#
 	for line in in_fd:
 		line = line.strip('\n')
@@ -39,8 +49,8 @@ def anonymise_dnsdenatex(in_csv, out_csv, salt):
 
 		fields = line.split(',')
 
-		if len(fields) != 11:
-			print 'Line {} has an invalid number of fields ({} != 11), skipping'.format(line_ct + 2, len(fields))
+		if len(fields) != field_count :
+			print 'Line {} has an invalid number of fields ({} != {}), skipping'.format(line_ct + 2, len(fields), field_count)
 			continue
 
 		# Initialise hash
@@ -50,14 +60,14 @@ def anonymise_dnsdenatex(in_csv, out_csv, salt):
 		md.update(salt)
 
 		# Anonymise the source IP
-		md.update(fields[1])
-		fields[1] = md.hexdigest()
+		md.update(fields[client_ip_index])
+		fields[client_ip_index] = md.hexdigest()
 
 		# Anonymise the query name; not clearing the hash object
 		# ensures that query names from other source IPs cannot
 		# be matched to the same hash value
-		md.update(fields[7])
-		fields[7] = md.hexdigest()
+		md.update(fields[qname_index])
+		fields[qname_index] = md.hexdigest()
 
 		out_fd.write('{}\n'.format(','.join(fields)))
 
